81578b6d2a81d6647f20b2cb3f5ec6f984ea813d
alxzhtkv <alxzhtkv@gmail.com>
2022-10-27 23:25:35 +0300

    add cleaning

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,28 +1,39 @@
-const initModalHandlers = function() {
-    document.querySelector('#nav__button_modal-open').addEventListener('click', openModal)    
+const initModalHandlers = function () {
+    document.querySelector('#nav__button_modal-open').addEventListener('click', openModal);
+    document.getElementById('section__modal_bottom-button-clear').addEventListener('click', clearStorage);
 }
 
 const openModal = function () {
     let modal = document.getElementById('section__modal');
+    modal.style.display = "none";
     let close_modal = document.getElementById('section__modal_header-сlose');
 
-    modal.style.display = "block";
 
-    let table = document.getElementById("section__table");
-    table.innerHTML = "";
+
+   
     let results = JSON.parse(localStorage.getItem("arr"));
+    if (!results) {
+        modal.style.display = "none";
+        const confirmSubmit = confirm('Список проголосовавших пуст');
+    }
+    else {
+        let table = document.getElementById("section__table");
+        table.innerHTML = "";
+        modal.style.display = "block";
+        results.forEach((result) => {
+            let tr = document.createElement("tr");
+            for (let key in result) {
+                let td = document.createElement("td");
+                td.innerHTML = result[key];
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        })
 
-    results.forEach((result) => {
-        let tr = document.createElement("tr");
-        for (let key in result) {
-            let td = document.createElement("td");
-            td.innerHTML = result[key];
-            tr.appendChild(td);
-        }
-        table.appendChild(tr);
-    })
+        generateDataList(results);
 
-    generateDataList(results);
+    }
+
 
     close_modal.onclick = function () {
         modal.style.display = "none";
@@ -35,7 +46,7 @@ const openModal = function () {
     }
 }
 
-const generateDataList = function(users) {
+const generateDataList = function (users) {
     let datalist = document.querySelector("#section__datalist");
     datalist.innerHTML = "";
     datalist.style.display = "block";
@@ -43,10 +54,13 @@ const generateDataList = function(users) {
     for (let user of users) {
         let option = document.createElement('option');
         option.value = user.name;
-        
+
         datalist.appendChild(option);
     }
 }
-
+const clearStorage = function () {
+    localStorage.removeItem("arr");
+    
+}
 document.addEventListener("DOMContentLoaded", initModalHandlers);
 
